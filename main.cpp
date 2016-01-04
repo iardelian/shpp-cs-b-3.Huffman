9342f21269f286ca18b3147a9d3eb715caf47b18
iardelyan <ardelyan.i@gmail.com>
2016-01-05 00:11:29 +0200

    Fix some mistakes and cope with functions names

--- main.cpp
+++ main.cpp
@@ -4,22 +4,21 @@
 #include <list>
 #include <fstream>
 
-
 using namespace std;
 
 class Node
 {
 public:
-    int a; //number
-    char c; //symbol (only for leaves)
+    int frequency; //frequency of sign in the file
+    char symbol; //value (only for leaves)
     Node *left, *right; // pointers to the left and right son
 
     Node(){left=right=NULL;} // constructor overload
 
-    Node (Node *L, Node *R){ // constructor takes a reference to the left and right son
-        left = L;
-        right = R;
-        a = L->a + R->a; // the sum of two numbers
+    Node (Node *Left, Node *Right){ // constructor takes a reference to the left and right son
+        left = Left;
+        right = Right;
+        frequency = Left->frequency + Right->frequency; // the sum of two numbers
     }
     ~Node(){ // destructor
         delete left;
@@ -27,66 +26,66 @@ public:
     }
 };
 
-struct MyCompare{
-    bool operator()(Node* l, Node* r){ //parameters overload
-        return l->a < r->a;
+struct MyCompare{ // for sorting elements in list
+    bool operator()(Node* left, Node* right){ //parameters overload
+        return left->frequency < right->frequency;
     }
 };
 
 //global variables
-map<char, vector<bool> > table;//code with symbol association (table for coding)
-Node *root; // root of a binary tree
+map <char, vector <bool> > symbolCodeTable;//table with code and symbol association (table for coding)
+Node *treeRoot; // root of a binary tree
 
-vector<bool> code; // vector of zeros and ones (only for BuildTable)
+vector <bool> code; // vector of zeros and ones (only for symbolCodeAssociation)
 
 /* Associate a symbol with its code starting from the root
  * and passing through the tree
  */
 
-void BuildTable(Node *root1){
-    if (root1->left!=NULL){
+void symbolCodeAssociation(Node *root){
+    if (root->left!=NULL){
         code.push_back(0); // put zero in the vector
-        BuildTable(root1->left); // run the function for the left son
+        symbolCodeAssociation(root->left); // run the function for the left son
     }
-    if (root1->right!= NULL){
+    if (root->right!= NULL){
         code.push_back(1);// put one in the vector
-        BuildTable(root1->right);// run the function for the right son
+        symbolCodeAssociation(root->right);// run the function for the right son
     }
-    if(root1->left==NULL && root1->right==NULL){ // if find a node with the letter
-        table[root1->c]=code; // associate symbol with the code
+    if(root->left==NULL && root->right==NULL){ // if find a node with the letter
+        symbolCodeTable[root->symbol]=code; // associate symbol with the code
     }
     code.pop_back();// reducing code by one
 
 }
 
-void Initialize(const map <char, int> &m)
+void buildTree(const map <char, int> &symbolFrequencyAssociation)
 {
-    list <Node*> t; // list of pointers to the node
+    list <Node*> pointer; // list of pointers to the node
 
-    for (map <char, int>::const_iterator iter = m.begin(); iter != m.end(); ++iter){ // go through map and load nodes
-        Node *p = new Node; // create a new node
-        p->c = iter->first;
-        p->a = iter->second;
-        t.push_back(p); // add a pointer to the list
+    for (map <char, int>::const_iterator iter = symbolFrequencyAssociation.begin(); iter != symbolFrequencyAssociation.end(); ++iter){ // go through map and load nodes
+        Node *newNode = new Node; // create a new node
+        newNode->symbol = iter->first;
+        newNode->frequency = iter->second;
+        pointer.push_back(newNode); // put pointer to the list
     }
 
-    while (t.size()!=1){ // while there will be one element
-        t.sort(MyCompare());
+    while (pointer.size()!=1){ // while there will be one element
+        pointer.sort(MyCompare());
 
-        Node *SonL = t.front(); // take the first element
-        t.pop_front(); // delete it
-        Node *SonR = t.front(); // take the second element
-        t.pop_front(); // delete it
+        Node *leftSon = pointer.front(); // take the first element
+        pointer.pop_front(); // delete it
+        Node *rightSon = pointer.front(); // take the second element
+        pointer.pop_front(); // delete it
 
-        Node *parent = new Node(SonL,SonR);// send to the constructor
-        t.push_back(parent);// put parent to the constructor
+        Node *parent = new Node(leftSon, rightSon);// send to the constructor and find parent value
+        pointer.push_back(parent);// put parent to the list
 
     }
-    root = t.front(); // pointer to the root
+    treeRoot = pointer.front(); // pointer to the root
 
     code.clear();
-    table.clear();
-    BuildTable(root); // creating a pair of character-code
+    symbolCodeTable.clear();
+    symbolCodeAssociation(treeRoot); // creating a pair of character-code
 }
 //function prototypes
 void archive(); // encoding function
@@ -96,7 +95,7 @@ int main ()
 {
     int choice;
     cout<<"*-*-*-*-*-*-*-*_Huffman Archiver_*-*-*-*-*-*-* \n"<<endl;
-    cout<<"    __ __ __ __\n   /           /|\n  /   *.huf   / |\n /__ __ __ __/ /|\n|__ __ __ __|/ /|\n|__ __ __ __|/ /\n|__ __ __ __|/\n"<<endl;
+    cout<<"    __ __ __ __\n   /           /|\n  /archive.huf/ |\n /__ __ __ __/ /|\n|__ __ __ __|/ /|\n|__ __ __ __|/ /\n|__ __ __ __|/\n"<<endl;
     cout<<"1.File archiving "<<endl;
     cout<<"2.Unzip the file \n"<<endl;
     cout<<"Please, make your choice: ";
@@ -124,102 +123,113 @@ int main ()
 
 void archive()
 {
-    ifstream f("file.txt", ifstream::binary); //input file
+    ifstream inputFile("file.txt", ifstream::binary); //input file
 
-    map <char,int> m;
-    int c, file_size = 0;
-    while((c = f.get()) != EOF) // until not the end of file get one byte
+    map <char,int> symbolFrequencyAssociation;
+    int frequency;
+    int file_size = 0;
+    while((frequency = inputFile.get()) != EOF) // until not the end of file, get one byte
     {
-        m[c]++;
+        symbolFrequencyAssociation[frequency]++;
         ++file_size;
     }
 
-    Initialize(m);
+    buildTree(symbolFrequencyAssociation);
 
-    f.clear(); f.seekg(0); // move the pointer to the beginning of the file (as it is at the moment at the end)
+    inputFile.clear();
+    inputFile.seekg(0); // move the pointer to the beginning of the file (as it is at the moment at the end)
 
-    ofstream g("archive.huf", ios::binary);
+    ofstream outputArchive("archive.huf", ios::binary);
 
-    g.write(reinterpret_cast<char *>(&file_size), sizeof(file_size)); // write the file size
-    unsigned short sz = m.size();
-    g.write(reinterpret_cast<char *>(&sz), sizeof(sz)); // the number of symbols
+    outputArchive.write(reinterpret_cast<char *>(&file_size), sizeof(file_size)); // write the file size
+    char size = symbolFrequencyAssociation.size();
+    outputArchive.write(reinterpret_cast<char *>(&size), sizeof(size)); // the number of symbols
     // write symbols with the frequencies
-    for (map<char, int>::iterator iter = m.begin(); iter != m.end(); ++iter)
+    for (map <char, int>::iterator iterator = symbolFrequencyAssociation.begin(); iterator != symbolFrequencyAssociation.end(); ++iterator)
     {
-        char fr = iter->first;
-        int sc = iter->second;
-        g.write(reinterpret_cast<char *>(&fr), sizeof(char)); // typecast to char *
-        g.write(reinterpret_cast<char *>(&sc), sizeof(int));
+        char first = iterator->first;
+        int second = iterator->second;
+        outputArchive.write(reinterpret_cast<char *>(&first), sizeof(char)); // typecast to char *
+        outputArchive.write(reinterpret_cast<char *>(&second), sizeof(int));
     }
 
     int count=0; // counter
     char buf=0;
     while (true)
     {
-        char c = f.get(); // get one byte
-        if (f.eof()) break; // if end of file - out of cycle
-        vector<bool> x = table[c];
-        for (int n=0; n<x.size(); n++){
-            buf = buf | x[n]<<(7-count); // vector of 0 and 1 put to byte
+        char byte = inputFile.get(); // get one byte
+        if (inputFile.eof()){
+        break; // if end of file - out of cycle
+        }
+        vector <bool> bin = symbolCodeTable[byte];// read code of symbol
+        for (int i=0; i<bin.size(); i++){
+            buf = buf | bin[i]<<(7-count); // check if it's 0 or 1 and put it to byte
             count++;
             if(count==8){
                 count = 0;
-                g<<buf;
+                outputArchive<<buf; // wright code into file
                 buf=0;
             }
         }
     }
-    if (count != 0) g << buf; // put last not full byte
-    f.close();
-    g.close();
+    if (count != 0) outputArchive << buf; // put last not full byte
+    inputFile.close();
+    outputArchive.close();
 
-    delete root; // free up memory
+    delete treeRoot; // free up memory
 }
 
+
+
+
 void dearchive(){
-    ifstream A("archive.huf", ios::in | ios::binary); // Archive file
-    ofstream O("file_dec.txt", ios::out | ios::binary);// Output file
+    ifstream inputArchive("archive.huf", ios::in | ios::binary); // archive file
+    ofstream outputFile("file_dec.txt", ios::out | ios::binary);// deachive file
 
     int file_size;
-    A.read(reinterpret_cast<char *>(&file_size), sizeof(file_size)); // read the file size
+    inputArchive.read(reinterpret_cast<char *>(&file_size), sizeof(file_size)); // read the file size
 
-    map <char,int> m;
-    unsigned short sz;
-    A.read(reinterpret_cast<char *>(&sz), sizeof(sz)); // количество символов
+    map <char,int> symbolFrequencyAssociation;
+    char size;
+    inputArchive.read(reinterpret_cast<char *>(&size), sizeof(size)); // typecast to char *
     // read symbols with the frequencies
-    for (int i = 0; i < sz; ++i)
+    for (int i = 0; i < size; ++i)
     {
-        char fr;
-        int sc;
-        A.read(reinterpret_cast<char *>(&fr), sizeof(char));
-        A.read(reinterpret_cast<char *>(&sc), sizeof(int));
-        m[fr] = sc;
+        char first;
+        int second;
+        inputArchive.read(reinterpret_cast<char *>(&first), sizeof(char));
+        inputArchive.read(reinterpret_cast<char *>(&second), sizeof(int));
+        symbolFrequencyAssociation[first] = second;
     }
 
-    Initialize(m);
+    buildTree(symbolFrequencyAssociation);
 
-    Node *p = root; // pointer to the root
+    Node *pointer = treeRoot; // pointer to the root
     int count=0;
     char byte;
-    byte = A.get(); // get byte
-    while(!A.eof()){
-        bool b = byte & (1 << (7-count));
-        if (b) p=p->right; else p=p->left;
-        if(p->left==NULL && p->right==NULL){
-            O << p->c;
+    byte = inputArchive.get(); // get byte
+    while(!inputArchive.eof()){
+        if (byte & (1 << (7-count))) { // check if it's 0 or 1
+            pointer = pointer->right;
+        }else{
+            pointer = pointer->left;
+        }
+        if (pointer->left==NULL && pointer->right==NULL) {
+            outputFile << pointer->symbol;
+
             if(--file_size == 0){
-            break; // all characters are encoded - stop decoding
+                break; // all characters are encoded - stop decoding
             }
-            p = root;
+            pointer = treeRoot;
         }
         count++;
         if (count==8){ // when 8 - get new byte
             count=0;
-            byte = A.get();
+            byte = inputArchive.get();
         }
     }
-    A.close();
-    O.close();
+    inputArchive.close();
+    outputFile.close();
 
-    delete root; // free up memory
+    delete treeRoot; // free up memory
 }
